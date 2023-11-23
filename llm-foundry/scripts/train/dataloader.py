4fd3c4f127c80b768bb0ca0e9157711379cf364b
Thạch Đức Long <65094224+longtd2910@users.noreply.github.com>
2023-11-23 03:18:21 +0000

    finalize first version of dataloader

--- llm-foundry/scripts/train/dataloader.py
+++ llm-foundry/scripts/train/dataloader.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Union
 
 def multiple_choice(inp: Dict[str, Union[str, List[str], int]]) -> Dict[str, str]:
     return {
-        "prompt" : 'Hãy trả lời câu hỏi chỉ với một đáp án. Ví dụ: A.Nội dung đáp án Trả lời: A. Câu hỏi:{query} Lựa chọn:{options} Đáp án: '.format(
+        "prompt" : 'Lựa chọn đáp án đúng. Câu hỏi: {query}.Lựa chọn: {options} '.format(
             query=inp["question"],
             options=" ".join(inp['choices']),
         ),
